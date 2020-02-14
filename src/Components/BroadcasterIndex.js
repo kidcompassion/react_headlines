068bd67c0e2e6e7cf86cb340b3049bafa33a39e0
kidcompassion <sallypoulsen@gmail.com>
2020-02-14 16:29:57 -0700

    Adds components.

---
+++ src/Components/BroadcasterIndex.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import BroadcasterStory from './BroadcasterStory';
+
+export default class BroadcasterIndex extends Component {
+
+    render() {
+
+        return (
+            <div className='row'>
+            test
+                <BroadcasterStory />
+            </div>
+        );
+    }
+}
